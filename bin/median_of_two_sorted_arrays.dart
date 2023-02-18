ec50ff3a3135927de7540a45ab4c90413ced0c7e
Mohamed Fouad <m.elsayedfouad99@gmail.com>
2023-02-18 17:00:58 +0200

    leetCode

--- bin/median_of_two_sorted_arrays.dart
+++ bin/median_of_two_sorted_arrays.dart
@@ -5,13 +5,15 @@ void main() {
 double findMedianSortedArrays(List<int> nums1, List<int> nums2) {
   nums1 += nums2;
   nums1.sort();
+  if (nums1.length == 1) {
+    return nums1[0].toDouble();
+  }
+
   if (nums1.length % 2 != 0) {
     return nums1[(nums1.length / 2).floor()].toDouble();
   } else {
     return (nums1[(nums1.length / 2).round()] +
             nums1[((nums1.length) / 2).round() - 1]) /
         2;
-    // (nums1[(nums1.length / 2).round()].toDouble()) +
-    //     (nums1[(nums1.length / 2).round() - 1].toDouble()) / 2;
   }
 }
