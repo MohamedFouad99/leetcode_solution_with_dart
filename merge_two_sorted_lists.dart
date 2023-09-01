43b806481ffb0251c730e0bb57094e0ca6772f8d
mohamed fouad <testing@bayanatz.com>
2023-09-01 20:02:27 +0300

    leet_code

--- merge_two_sorted_lists.dart
+++ merge_two_sorted_lists.dart
@@ -3,8 +3,8 @@ void main() {
 }
 
 List<int> mergeTwoLists(List<int> list1, List<int> list2) {
-  if (list1 == null) return list2;
-  if (list2 == null) return list1;
+  if (list1.isEmpty) return list2;
+  if (list2.isEmpty) return list1;
   list1.addAll(list2);
   list1.sort();
   return list1;
