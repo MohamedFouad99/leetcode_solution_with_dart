92d11245523f54f3fa13134eb8b5411f48c41d8a
Mohamed Fouad <132960891+mohamedFouad799@users.noreply.github.com>
2025-01-21 12:39:26 +0200

    Add implementation for lemonade change problem

---
+++ lemonade_change.dart
@@ -0,0 +1,29 @@
+void main() {
+  print(lemonadeChange([5, 5, 5, 10, 20]));
+}
+
+bool lemonadeChange(List<int> bills) {
+  int five = 0, ten = 0;
+  for (int i = 0; i < bills.length; i++) {
+    if (bills[i] == 5) {
+      five++;
+    } else if (bills[i] == 10) {
+      if (five > 0) {
+        five--;
+        ten++;
+      } else {
+        return false;
+      }
+    } else {
+      if (five > 0 && ten > 0) {
+        five--;
+        ten--;
+      } else if (five >= 3) {
+        five -= 3;
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
+}
