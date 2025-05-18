5665ea969d6b2ca62ec6cd02630d5783dbaf8b32
Mohamed Fouad <73003226+MohamedFouad99@users.noreply.github.com>
2025-05-18 22:10:56 +0300

    Add implementation for finding the maximum consecutive ones in a binary array

---
+++ max_consecutive_ones.dart
@@ -0,0 +1,16 @@
+void main() {}
+int findMaxConsecutiveOnes(List<int> nums) {
+  int count = 0;
+  int max = 0;
+  for (int i = 0; i < nums.length; i++) {
+    if (nums[i] == 1) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > max) {
+      max = count;
+    }
+  }
+  return max;
+}
